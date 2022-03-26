8e73abe08b86677f1868b7d602c63301cdcb30ea
PetrMKO <GitHub_govno0912>
2022-03-26 15:33:56 +0300

    Поиск работает, середина определяется по выборке точек

--- src/main/resources/static/scripts/map.js
+++ src/main/resources/static/scripts/map.js
@@ -26,33 +26,107 @@ document.addEventListener('DOMContentLoaded', () => {
                 geoObjectOpenBalloonOnClick: true,
                 clusterOpenBalloonOnClick: false
             });
+
         Map.geoObjects.add(objectManager);
-        // objectManager.add(obj);
+
+
+        console.log(namePointsArr);
+
+
+
+        function CustomSearchProvider(points) {
+            this.points = points;
+        }
+
+// Провайдер ищет по полю text стандартным методом String.ptototype.indexOf.
+        CustomSearchProvider.prototype.geocode = function (request, options) {
+            var deferred = new ymaps.vow.defer(),
+                geoObjects = new ymaps.GeoObjectCollection();
+
+            var points = [];
+
+            // Ищет в свойстве text каждого элемента массива.
+            for (var i = 0, l = this.points.length; i < l; i++) {
+                let point = this.points[i];
+                console.log('search');
+                if (point.properties.hintContent.toLowerCase().indexOf(request.toLowerCase()) != -1) {
+                    points.push(point);
+                }
+            }
+
+            // Добавляет точки в результирующую коллекцию.
+            for (var i = 0, l = points.length; i < l; i++) {
+                var point = points[i],
+                    coords = point.geometry.coordinates,
+                    text = point.properties.hintContent;
+
+                geoObjects.add(new ymaps.Placemark(coords, {
+                    name: text,
+                    description: '',                        //Заглушка для описания, по идее должно быть месторождение
+                    balloonContentBody: '<p>' + text + '</p>',
+                    boundedBy: [coords, coords]
+                }));
+            }
+
+            deferred.resolve({
+                // Геообъекты поисковой выдачи.
+                geoObjects: geoObjects,
+                // Метаинформация ответа.
+                metaData: {
+                    geocoder: {
+                        // Строка обработанного запроса.
+                        request: request,
+                        // Количество найденных результатов.
+                        found: geoObjects.getLength()
+                    }
+                }
+            });
+
+            // Возвращает объект-обещание.
+            return deferred.promise();
+        };
+
 
 
         $.ajax({
-            url: '../static/scripts/jsons/getpoints.json'
+            url: '../testjsons/push.json'
         }).done(function(data) {
-            namePointsArr = data.features
             objectManager.add(data);
+            console.log(data.features);
+            namePointsArr = data.features
+
+            let bounds = objectManager.getBounds();
+            console.log(bounds);
+            bounds = [bounds[0][0] + (bounds[1][0] - bounds[0][0])/2,
+                bounds[0][1] + (bounds[1][1] - bounds[0][1])/2]
+
+            console.log(bounds)
+            Map.setCenter(bounds, 8, {
+                checkZoomRange: true
+            });
+
+            // Map.geoObjects.add(new ymaps.Placemark(bounds, {
+            //     balloonContent: '<strong>центр</strong>'
+            // }, {
+            //     preset: 'islands#icon',
+            //     iconColor: 'red'
+            // }))
+
+            // Добавляем контрол в верхний левый угол,
+            let mySearchControl = new ymaps.control.SearchControl({
+                options: {
+                    // Заменяем стандартный провайдер данных (геокодер) нашим собственным.
+                    provider: new CustomSearchProvider(namePointsArr),
+                    float: "left",
+                    // Не будем показывать еще одну метку при выборе результата поиска,
+                    // т.к. метки коллекции myCollection уже добавлены на карту.
+                    noPlacemark: true,
+                    resultsPerPage: 5
+                }});
+
+            Map.controls.add(mySearchControl);
+            console.log(mySearchControl);
         });
-
-
-
-
-        let mySearchControl = new ymaps.control.SearchControl({
-            options: {
-                // Заменяем стандартный провайдер данных (геокодер) нашим собственным.
-                provider: new CustomSearchProvider(pointsArr),
-                float: "left",
-                // Не будем показывать еще одну метку при выборе результата поиска,
-                // т.к. метки коллекции myCollection уже добавлены на карту.
-                noPlacemark: true,
-                resultsPerPage: 5
-            }});
-
-        // Добавляем контрол в верхний правый угол,
-        Map.controls.add(mySearchControl);
 
 
         function loadBalloonData (id) {
@@ -68,7 +142,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             function resolveData () {
                 let infoBlock = `<div class="preInfoField">&#9679 Точка ${hint}</div><ul class="pointInfoList">`
-                fetch(`../static/scripts/jsons/getDiscription.json`)
+                fetch(`../testjsons/getDiscription.json`)
                     .then(data => data.json())
                     .then(data => {
                         for(let key in data){
@@ -110,114 +184,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     ymaps.ready(init);
-
-
-    // fetch('https://geocode-maps.yandex.ru/1.x/?format=json&apikey=74bf9661-b70c-4d9e-875c-fc4500cf3256=55.689941211,54.484870589&kind=hydro&results=1')
-    //     .then(data => {
-    //         console.log(data.json())
-    //     })
-
-    //aef71caa-e287-4cde-a13b-e6a23db70fd8
-    //https://geocode-maps.yandex.ru/1.x/?format=json&apikey=aef71caa-e287-4cde-a13b-e6a23db70fd8&geocode=Москва, улица Новый Арбат, дом 24
-        // .then(data => data.json())
-        // .then(function (data) {
-        //     data.Holes.forEach((item, key) => {
-        //         namePointsArr.push(item.HoleNumber);
-        //         coords = item.gps.split(',');
-        //         coords[0] = +coords[0];
-        //         coords[1] = +coords[1];
-        //
-        //         obj.features.push(
-        //             {
-        //                 type: "Feature",
-        //                 id: key,
-        //                 geometry: {
-        //                     type: "Point",
-        //                     coordinates: coords
-        //                 },
-        //                 properties: {
-        //                     hintContent: item.HoleNumber
-        //                 }
-        //             }
-        //         )
-        //     })
-        //     pointsArr = obj.features;
-        //     console.log(pointsArr);
-        //
-        //
-        //     console.log(JSON.stringify(obj));
-        //
-        //     return JSON.stringify(obj)
-        // }).then(data => {
-        //     obj = data;
-        //     ymaps.ready(init);
-        // })
-
-
-
-
-    function CustomSearchProvider(points) {
-        this.points = points;
-    }
-
-// Провайдер ищет по полю text стандартным методом String.ptototype.indexOf.
-    CustomSearchProvider.prototype.geocode = function (request, options) {
-        let deferred = new ymaps.vow.defer(),
-            geoObjects = new ymaps.GeoObjectCollection(),
-            // Сколько результатов нужно пропустить.
-            offset = options.skip || 0,
-            // Количество возвращаемых результатов.
-            limit = options.results || 20;
-
-        let points = [];
-        // Ищем в свойстве text каждого элемента массива.
-        for (let i = 0, l = this.points.length; i < l; i++) {
-            let point = this.points[i];
-            if (point.properties.hintContent.toLowerCase().indexOf(request.toLowerCase()) != -1) {
-                points.push(point);
-            }
-        }
-        // При формировании ответа можно учитывать offset и limit.
-        points = points.splice(offset, limit);
-        // Добавляем точки в результирующую коллекцию.
-        for (let i = 0, l = points.length; i < l; i++) {
-            let point = points[i],
-                coords = point.geometry.coordinates,
-                text = point.properties.hintContent;
-
-            geoObjects.add(new ymaps.Placemark(coords, {
-                name: text + ' name',
-                description: text + ' description',
-                balloonContentBody: '<p>' + text + '</p>',
-                boundedBy: [coords, coords]
-            }));
-        }
-
-        deferred.resolve({
-            // Геообъекты поисковой выдачи.
-            geoObjects: geoObjects,
-            // Метаинформация ответа.
-            metaData: {
-                geocoder: {
-                    // Строка обработанного запроса.
-                    request: request,
-                    // Количество найденных результатов.
-                    found: geoObjects.getLength(),
-                    // Количество возвращенных результатов.
-                    results: limit,
-                    // Количество пропущенных результатов.
-                    skip: offset
-                }
-            }
-        });
-
-        // Возвращаем объект-обещание.
-        return deferred.promise();
-    };
-
-
-
-
 
 
     let closeSBBtn = document.querySelector(".expandSidebarBtn")
